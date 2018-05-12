0a05b62a9d520f598f5ada02213b62a408d4c4e2
zubair <zubairabuhasan@gmail.com>
2018-05-12 15:38:44 +0800

    List payments using Dapper

---
+++ AspNetMvcCourse/Controllers/PaymentsController.cs
@@ -0,0 +1,30 @@
+﻿using AspNetMvcCourse.Models;
+using AspNetMvcCourse.Services;
+using Dapper;
+using System.Web.Mvc;
+
+namespace AspNetMvcCourse.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        // GET: Payments
+        public ActionResult Index(int id)
+        {
+            // Get payments by students by id
+            using (var c = ConnectionHelper.GetConnection())
+            {
+                const string sql = @"SELECT * FROM Payments a 
+                JOIN Students b ON b.Id = a.StudentId 
+                WHERE StudentId = @StudentId";
+
+                var payments = c.Query<Payment, Student, Payment>(sql, (payment, student) =>
+                    {
+                        payment.Student = student;
+                        return payment;
+                    }, new { StudentId = id });
+
+                return View(payments);
+            }
+        }
+    }
+}
---
+++ AspNetMvcCourse/Services/ConnectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AspNetMvcCourse.Services
+{
+    public static class ConnectionHelper
+    {
+        public static IDbConnection GetConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["AspNetMvcDbContext"].ToString();
+            return new SqlConnection(connectionString);
+        }
+
+        public static IDbConnection GetConnection(string connectionString)
+        {
+            return new SqlConnection(connectionString);
+        }
+
+    }
+}
